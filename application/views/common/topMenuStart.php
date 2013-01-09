7fe91a840122dc7be9e27d7fed5088ed2c63e881
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-09 18:52:36 -0200

    tarefa 26 - Diferenciar navbar de producao e homologacao

--- application/views/common/topMenuStart.php
+++ application/views/common/topMenuStart.php
@@ -1,4 +1,4 @@
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar <?php if (ENVIRONMENT == "production") echo "navbar-inverse"; ?> navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container-fluid">
 
