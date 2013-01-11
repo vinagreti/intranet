cd01da4e318a734ffcdcec9ff465ae1860378f5d
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-11 10:15:07 -0200

    tarefa 68 - Colocar title nos botões do filtro

--- application/views/task/list.php
+++ application/views/task/list.php
@@ -3,9 +3,9 @@
 	<h3>Tarefas</h3>
 
 	<h5>
-		<a class="taskListFilter btn btn-primary" href="#"><i class="icon-search icon-white"></i> </a>
-		<a class="taskFilterButton btn btn-info" href="#"><i class="icon-refresh icon-white"></i></a>
-		<a class="taskListFilterReset btn btn-danger" href="#"><i class="icon-remove icon-white"></i></a>
+		<a class="taskListFilter btn btn-primary" href="#" rel="tooltip" title="Filtro"><i class="icon-search icon-white"></i> </a>
+		<a class="taskFilterButton btn btn-info" href="#" rel="tooltip" title="Atualizar lista"><i class="icon-refresh icon-white"></i></a>
+		<a class="taskListFilterReset btn btn-danger" href="#" rel="tooltip" title="Limpar filtro"><i class="icon-remove icon-white"></i></a>
 	</h5>
 
 
