6b672bcc07cc1e10277b3b3f855a1b68bd6cd270
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-11 11:31:20 -0200

    tarefa 71 - Botão abrir tarefa abre a tarefa em uma nova janela, evitando perder o filtro

--- application/views/task/ajaxSearch.php
+++ application/views/task/ajaxSearch.php
@@ -65,7 +65,7 @@
 				</ul>
 			</td>
 
-			<td class="center"><small><a href="<?=base_url()?>task/view/<?=$task->taskID?>" rel="tooltip" title="<?=$task->taskTitle?>"><?=substr($task->taskTitle, 0, 80)?></a></small></td>
+			<td class="center"><small><a href="<?=base_url()?>task/view/<?=$task->taskID?>" target="_blank" rel="tooltip" title="<?=$task->taskTitle?>"><?=substr($task->taskTitle, 0, 80)?></a></small></td>
 			<td class="center"><small><?=substr($task->taskResponsableName, 0, 20)?></small></td>
 			<td class="center"><small><?=substr($task->taskKindName, 0, 20)?></small></td>
 			<td class="center"><small><?=substr($task->taskFather, 0, 30)?></small></td>
