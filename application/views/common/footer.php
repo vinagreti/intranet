f72c80ecaa381c7e2cc96b8213500c1bee525f5e
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-14 11:17:32 -0200

    task 73 - finished

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -3,6 +3,6 @@
 <hr>
 
 <footer>
-	<p class="pull-left">&copy; <a target="_blank" href="<?=base_url()?>">Tzadi.com</a> 2012</p>
+	<p class="pull-left">&copy; <a target="_blank" href="<?=base_url()?>">Tzadi.com</a> 2013</p>
 	<p class="pull-right">Powered by: <a target="_blank" href="<?=base_url()?>">Tzadi</a></p>
 </footer>
