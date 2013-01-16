aaf5794dbac9bb493b9e49c0ff8e813329d66db7
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-16 20:16:54 -0200

    task 96 - finished

--- application/views/common/bottomHTML.php
+++ application/views/common/bottomHTML.php
@@ -1,21 +1,25 @@
-	</div> <!-- container-fluid -->
+		</div> <!-- container-fluid -->
 
-	  <!-- Le javascript
-	  ================================================== -->
-	  <!-- Placed at the end of the document so the pages load faster -->
-	  <script src="<?=base_url()?>assets/JQuery/jquery.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-transition.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-alert.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-modal.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-dropdown.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-scrollspy.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-tab.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-tooltip.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-popover.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-button.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-collapse.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-carousel.js"></script>
-	  <script src="<?=base_url()?>assets/bootstrap/js/bootstrap-typeahead.js"></script>
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="<?=base_url()?>assets/JQuery/jquery.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-transition.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-alert.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-modal.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-dropdown.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-scrollspy.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-tab.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-tooltip.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-popover.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-button.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-collapse.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-carousel.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-typeahead.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-daterangepicker.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-daterangepicker-date.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-datepicker.js"></script>
+		<script src="<?=base_url()?>assets/bootstrap/js/bootstrap-clockface.js"></script>
 
-</body>
+	</body>
 </html>
--- application/views/common/topHTML.php
+++ application/views/common/topHTML.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <html>
-<head>
-	<meta charset="utf-8">
+	<head>
+		<meta charset="utf-8">
+		<title>Tzadi</title>
+		<!-- Enable responsive features-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap CSS-->
+		<link href="<?=base_url()?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<!-- Bootstrap CSS responsive-->
+		<link href="<?=base_url()?>assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<!-- Bootstrap clockface CSS-->
+		<link href="<?=base_url()?>assets/bootstrap/css/bootstrap-clockface.css" rel="stylesheet" media="screen">
+		<!-- Bootstrap daterangepicker CSS-->
+		<link href="<?=base_url()?>assets/bootstrap/css/bootstrap-daterangepicker.css" rel="stylesheet" media="screen">
+		<!-- Bootstrap datepicker CSS-->
+		<link href="<?=base_url()?>assets/bootstrap/css/bootstrap-datepicker.css" rel="stylesheet" media="screen">
+		<!-- start: Favicon -->
+		<link rel="shortcut icon" href="<?= base_url()?>assets/img/favicon.ico">
+		<!-- Putting the container under the navbar -->
+		<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
+		.sidebar-nav {
+			padding: 9px 0;
+		}
+		</style>
+	</head>
 
-	<!-- Enable responsive features-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<title>Tzadi</title>
-	<!-- Bootstrap CSS-->
-	<link href="<?=base_url()?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-	<!-- Bootstrap CSS responsive-->
-	<link href="<?=base_url()?>assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-
-	<!-- start: Favicon -->
-	<link rel="shortcut icon" href="<?= base_url()?>assets/img/favicon.ico">
-	<!-- end: Favicon -->
-
-	<!-- Putting the container under the navbar -->
-	<style type="text/css">
-	body {
-		padding-top: 60px;
-		padding-bottom: 40px;
-	}
-	.sidebar-nav {
-		padding: 9px 0;
-	}
-	</style>
-
-</head>
-
-<body>
+	<body>
