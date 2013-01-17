61822658d4b9dbaac6ebcebb6399df9424128829
Bruno da Silva joão <brunodasj@gmail.com>
2013-01-17 14:26:56 -0200

    task 107 - finished

--- application/views/task/ajaxSearch.php
+++ application/views/task/ajaxSearch.php
@@ -72,7 +72,7 @@
 			<td class="center"><small><a href="<?=base_url()?>task/view/<?=$task->taskID?>" target="_blank" rel="tooltip" title="<?=$task->taskTitle?>"><?=substr($task->taskTitle, 0, 80)?></a></small></td>
 			<td class="center"><small><?=substr($task->taskResponsableName, 0, 20)?></small></td>
 			<td class="center"><small><?=substr($task->taskKindName, 0, 20)?></small></td>
-			<td class="center"><small><?=substr($task->taskLink, 0, 1)?><?=substr($task->taskFather, 0, 30)?></small></td>
+			<td class="center"><small><a href="<?=base_url()?>task/view/<?=$task->taskFather?>" target="_blank"><?=substr($task->taskLink, 0, 1)?><?=substr($task->taskFather, 0, 30)?></a></small></td>
 
 			<?php if ( $task->taskStatus == "1" ) $label = ""; ?>
 			<?php if ( $task->taskStatus == "2" ) $label = "label-inverse"; ?>
