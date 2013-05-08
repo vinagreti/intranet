dd402027fd386b59060032fa0355554feaad3526
root <root@ip-10-252-14-80.sa-east-1.compute.internal>
2013-05-08 16:49:15 -0300

    mudado nome do coockie pra nao dar conflito com o da tzadi

--- application/config/config.php
+++ application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = 'Dublin';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'tzadi';
+$config['sess_cookie_name']		= 'tzadiIntra';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
